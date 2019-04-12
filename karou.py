f73337eaeaf38d669fdc229294b6522a2d0bbeb8
wzhuge <wzhuge@paypal.com>
2019-04-12 16:02:55 +0800

    [Twitter API]

--- karou.py
+++ karou.py
@@ -4,3 +4,11 @@ from tsuita.utility import TweetAPI
 proxy = "https://sjd-entbc-001:80"
 twi = TweetAPI(proxy = proxy)
 api = twi.get_api()
+sample = api.search(q = "令和", )
+
+for tweet in sample:
+  print(tweet.user.id)
+  print(tweet.user.description)
+  
+  
+  
